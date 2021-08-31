ec47d0d734b7312f327c6ce69925c2a4d7052a57
Nekitoni4 <nekitgo80@gmail.com>
2021-08-31 15:11:41 +0300

    realize_methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    
+  def tests_on_this_difficulty(difficulty_level)
+    result_tests = []
+    TestUser.where("user_id = ?", self.id)
+      .map { |model| Test.find(model.test_id) }
+      .filter { |model| model.level == difficulty_level }
+  end
 end
